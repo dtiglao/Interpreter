694df88858b75f0d36b96df2568dc2d92eea1128
rosnyaleph <56274415+rcolas97@users.noreply.github.com>
2019-10-16 14:57:19 -0400

    TestProgram_v1

--- TestProgram.java
+++ TestProgram.java
@@ -7,23 +7,24 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class TestProgram {
-    public static String[] array = {"begin", "add", "in", "end"};
+    public static String[] array = {"begin", "and", "if", "end", "function", "elseif", "else", "for", "while", "then", "print"};
     public static List<String> keywords = Arrays.asList(array);
     public static int s_line = 0;
-    public static List<TokenRecord> bucket = new ArrayList<>(100);
+    public static List<TokenRecord> bucket = new ArrayList<>(200);
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(new File(".").getAbsoluteFile());
         File file = new File("src\\InputFiles\\basic_julia.txt");
         Scanner scan = new Scanner(file);
 
-        String[] array = {"begin", "add", "in", "end"};
-        List<String> keywords = Arrays.asList(array);
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
             s_line++;
             String[] tokens= line.split(" ");
             tokenCheck(tokens, bucket, s_line);
+        }
+
+        for (TokenRecord tr : bucket) {
+            tr.printRecord();
         }
     }
 
@@ -33,7 +34,15 @@ public class TestProgram {
             if (t.contains(";")) {
                 t = t.substring(0, t.indexOf(";"));
             }
-            if (t.contains("#") || t.contains(" ") || t == " ") return;
+            while (t.contains("\t")) {
+                t = checkForTabs(t);
+            }
+            // System.out.println(t);
+            if (t.contains("#") || t == " ") return;
+            else if (t.contains("\"")) {
+                token = new TokenRecord(t, "10", "String_Lit", srcLine);
+                tokenList.add(token);
+            }
             else if (t.matches("[0-9.]+")) {
                 if (t.contains(".")) {
                     token = new TokenRecord(t, "12", "Float_Lit", srcLine);
@@ -42,7 +51,13 @@ public class TestProgram {
                     token = new TokenRecord(t, "11", "Int_Lit", srcLine);
                     tokenList.add(token);
                 }
-            } else if (t.matches("[A-Za-z0-9_]+")) {
+            } else if (t.matches("[A-Za-z0-9_()]+")) {
+                if (t.contains("()")) {
+                    token = new TokenRecord(t, "30", "function", srcLine);
+                    tokenList.add(token);
+                }
+            }
+            else if (t.matches("[A-Za-z0-9_]+")) {
                 if (keywords.contains(t)) {
                     token = new TokenRecord(t, "13", "Keyword", srcLine);
                     tokenList.add(token);
@@ -77,17 +92,72 @@ public class TestProgram {
                         token = new TokenRecord(t, "05", "Power_Op", srcLine);
                         tokenList.add(token);
                         break;
+                    case ">":
+                        token = new TokenRecord(t, "06", "GT_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case ">=":
+                        token = new TokenRecord(t, "07", "GE_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "<":
+                        token = new TokenRecord(t, "08", "LT_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "<=":
+                        token = new TokenRecord(t, "09", "LE_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "==":
+                        token = new TokenRecord(t, "010", "EQ_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "!=":
+                        token = new TokenRecord(t, "011", "NE_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "\\":
+                        token = new TokenRecord(t, "012", "Rev_Div_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "%":
+                        token = new TokenRecord(t, "013", "Rem_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "+=":
+                        token = new TokenRecord(t, "014", "Inc_By_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "-=":
+                        token = new TokenRecord(t, "015", "Dec_By_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case "(":
+                        token = new TokenRecord(t, "016", "LP_Op", srcLine);
+                        tokenList.add(token);
+                        break;
+                    case ")":
+                        token = new TokenRecord(t, "017", "RP_Op", srcLine);
+                        tokenList.add(token);
+                        break;
                     default:
                         char[] charArray = t.toCharArray();
                         String[] array = new String[charArray.length];
                         int i = 0;
                         for (char x : charArray) {
                             array[i] = Character.toString(x);
+                            i++;
                         }
                         tokenCheck(array, tokenList, srcLine);
-
                 }
             }
         }
+    }
+
+    public static String checkForTabs(String s) {
+        if (s.contains("\t")) {
+            return s.substring(s.indexOf("\t") + 1);
+        }
+        return s;
     }
 }
